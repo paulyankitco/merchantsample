09036dc568a8b7a738d98eaa19378d378efb8795
Paul Yan <pyan@kitco.com>
2019-01-30 10:41:26 -0500

    aaa

---
+++ src/main/java/com/sample/util/Configuration.java
@@ -0,0 +1,47 @@
+package com.sample.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *  For a full list of configuration parameters refer in wiki page.(https://github.com/paypal/sdk-core-java/blob/master/README.md) 
+ */
+public class Configuration {
+	
+	// Creates a configuration map containing credentials and other required configuration parameters.
+	public static final Map<String,String> getAcctAndConfig(){
+		Map<String,String> configMap = new HashMap<String,String>();
+		configMap.putAll(getConfig());
+				
+		// Account Credential
+		configMap.put("acct1.UserName", "paypal-merchant-sandbox_api1.kitco.com");
+		configMap.put("acct1.Password", "G2TRSKFDE4DAX554");
+		configMap.put("acct1.Signature", "APHhTBfLdUY0OrALGtFdgv8c0MxqAz1Y1a7r4IPsOqIo.5rQUNDTvMfT");
+		// Subject is optional, only required in case of third party permission
+		//configMap.put("acct1.Subject", "");
+		
+		// Sample Certificate credential
+		// configMap.put("acct2.UserName", "certuser_biz_api1.paypal.com");
+		// configMap.put("acct2.Password", "D6JNKKULHN3G5B8A");
+		// configMap.put("acct2.CertKey", "password");
+		// configMap.put("acct2.CertPath", "resource/sdk-cert.p12");
+		// configMap.put("acct2.AppId", "APP-80W284485P519543T");
+		
+		return configMap;
+	}
+	
+	public static final Map<String,String> getConfig(){
+		Map<String,String> configMap = new HashMap<String,String>();
+		
+		// Endpoints are varied depending on whether sandbox OR live is chosen for mode
+		configMap.put("mode", "sandbox");
+		
+
+		// These values are defaulted in SDK. If you want to override default values, uncomment it and add your value.
+		// configMap.put("http.ConnectionTimeOut", "5000");
+		// configMap.put("http.Retry", "2");
+		// configMap.put("http.ReadTimeOut", "30000");
+		// configMap.put("http.MaxConnection", "100");
+		return configMap;
+	}
+}
